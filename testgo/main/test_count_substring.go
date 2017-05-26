aeda16c1b760f6f0108f4ec7cead21d9cd116ba2
zhangbin <zhangbincodec@gmail.com>
2017-05-26 11:42:49 +0800

    count str

---
+++ testgo/main/test_count_substring.go
@@ -0,0 +1,30 @@
+/*
+4.7.4 字符串替换
+
+Replace 用于将字符串 str 中的前 n 个字符串 old 替换为字符串 new，并返回一个新的字符串，如果 n = -1 则替换所有字符串 old 为字符串 new：
+
+strings.Replace(str, old, new, n) string
+4.7.5 统计字符串出现次数
+
+Count 用于计算字符串 str 在字符串 s 中出现的非重叠次数：
+
+strings.Count(s, str string) int
+*/
+
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	var str string = "Hello, how is it going, Hugo?"
+	var manyG = "gggggggggg"
+
+	fmt.Printf("Number of H's in %s is: ", str)
+	fmt.Printf("%d\n", strings.Count(str, "H"))
+
+	fmt.Printf("Number of double g's in %s is: ", manyG)
+	fmt.Printf("%d\n", strings.Count(manyG, "gg"))
+}
