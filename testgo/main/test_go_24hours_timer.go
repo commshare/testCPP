3d69ac16025a59b11811ae3363657d577d4e2ab1
zhangbin <zhangbincodec@gmail.com>
2017-05-27 17:56:20 +0800

    24hours timers

---
+++ testgo/main/test_go_24hours_timer.go
@@ -0,0 +1,34 @@
+package main
+
+/*https://github.com/jjjachyty/zlog/blob/master/task.go*/
+import (
+	"fmt"
+	"time"
+)
+
+//DailyRollingLogs func 每隔24小时执行一次任务
+func DailyRollingLogs(f func()) {
+	go func() {
+		for {
+
+			now := time.Now()
+			// 计算下一个零点
+			next := now.Add(time.Hour * 24)
+			//next := now.Add(time.Second * 10)
+			next = time.Date(next.Year(), next.Month(), next.Day(), 0, 0, 0, 0, next.Location())
+
+			t := time.NewTimer(next.Sub(now))
+			//t := time.NewTimer(time.Second * 10)
+			<-t.C
+			f()
+		}
+	}()
+}
+
+func log() {
+	fmt.Println("24hours....")
+}
+
+func main() {
+	DailyRollingLogs(log)
+}
